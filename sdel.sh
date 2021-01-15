5bf9786cc6a7cb70b7e7f70e3c486e24111e0765
D R Siddhartha <siddharthadr11@gmail.com>
2021-01-15 14:00:42 +0530

    add sdel.sh

--- sdel.sh
+++ sdel.sh
@@ -1,15 +1,41 @@
 #! /bin/bash
 
-# Steps:
-# 1) write sdel for single files first
-# 2) extend to directories
-# 3) extend to directories recursively
-# 4) write a periodic trash deleter
+# Solution to: https://tldp.org/LDP/abs/html/writingscripts.html (Safe Delete)
+
+SOURCE_PATH="$PWD/$(basename $BASH_SOURCE)"
+SOURCE_NAME="sdel"	# TODO: remove this hardcoded variable
 
 # check if trash dir exists otherwise create it
 if [ ! -d "$HOME/trash" ] ; then
 	mkdir "$HOME/trash"
 fi
+
+# check if cron contains trash cleanup; otherwise add it
+if ! crontab -l | grep "$SOURCE_NAME clean" >/dev/null ; then
+	crontab -l > temp >/dev/null
+	# run every Sunday at 10 pm
+	echo "0 22 * * sun \"$SOURCE_NAME\" clean" >> temp
+	crontab temp
+	rm temp
+fi
+
+# clean trash
+clean () {
+	# get time now
+	NOW=(date "+%s")
+
+	if [ -d "$HOME/trash" ] ; then
+		for file in "$HOME/trash"/* ; do
+			# get file's last modified time
+			FILE_NOW=(date -r "$file" "+%s")
+
+			# if file older than 48 hours(48*60*60 seconds) then delete file
+			if [ $(($NOW-$FILE_NOW>48*60*60)) ] ; then
+				rm "$file"
+			fi
+		done
+	fi
+}
 
 # sdel file
 sdel_file () {
@@ -37,14 +63,16 @@ sdel_dir () {
 		sdel_file "$path"
 	fi
 
-	# delete empty dir
+	# delete empty dir  <-- broken fix this
 	if [ -d "$path" ] ; then
 		rmdir "$path"
 	fi
 }
 
 
-if [ -d "$1" ] ; then
+if [ "$1" = "clean" ] ; then
+	clean
+elif [ -d "$1" ] ; then
 	sdel_dir "$1"
 else
 	sdel_file "$1"
